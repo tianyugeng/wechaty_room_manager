533e79e057f79f6be252a7ff3699dca493ec585b
tianyugeng <305122079@163.com>
2020-04-14 18:32:10 +0800

    添加初始逻辑
    为机器人添加功能:
    定义自定义群管理员
    删除自定义群管理员
    响应自定义群管理员的群公告指令
    发布群公告

--- examples/ding-dong-bot.ts
+++ examples/ding-dong-bot.ts
@@ -40,8 +40,100 @@ function onLogout (user: Contact) {
   log.info('StarterBot', '%s logout', user)
 }
 
+function addAdmin( roomId:string, userId:string) {
+  adminSetting[roomId][userId] = true
+}
+
+function delAdmin(roomId: string, userId: string) {
+  adminSetting[roomId][userId] = false
+}
+
 async function onMessage (msg: Message) {
   log.info('StarterBot', msg.toString())
+
+  const contact = msg.from()
+  if (!contact) {
+    return null
+  }
+  var userId: any
+  if (contact) {
+    userId = contact.id
+  }
+  
+  const text = msg.text()
+  const room = msg.room()
+  // 当由群组接收消息时,开始逻辑判断
+  if (room) {
+    // 获取群组名称和群组ID
+    const topic = await room.topic()
+    const roomId: any = room.id
+    // 获取群主
+    const owner = room.owner()
+    // 获取被@的用户列表
+    const contactList = await msg.mention()
+    console.log("获取艾特")
+    console.log(contactList)
+
+
+    // 判断是否为命令消息
+    if (text.search("^##") == 0) {
+      // 当目标群组未初始化时,执行初始化操作
+      // 只有群主可以设置/撤销管理员
+      console.log("测到群主")
+      console.log(owner)
+      console.log(userId)
+      if (!!userId && !!owner && owner.id == userId) {
+        console.log("检测到群主命令")
+        console.log(!adminSetting[roomId])
+        if (!adminSetting[roomId]) {
+          console.log("执行初始化")
+          //var roomTemp: { [key: string]: any } = {}
+          adminSetting[roomId] = {}
+        }
+        console.log("检测初始化")
+        console.log(adminSetting)
+        console.log(text.search("(?<=^##)ADD"))
+        // 处理命令 设置管理员
+        if (text.search("(?<=^##)ADD") > 0) {
+          // 将@的人员添加为管理员
+          console.log('添加管理员', contactList)
+          for (var targetUserId of contactList) {
+            addAdmin(roomId, targetUserId.id)
+            adminSetting[roomId][userId] = true
+          }
+          return null
+        }
+
+        // 处理命令 撤销管理员
+        if (text.search("(?<=^##)DEL") > 0) {
+          // 将@的人员从管理员名单中剔除
+          console.log('delAdmin', contactList)
+          for (var targetUserId of contactList) {
+            delAdmin(roomId, targetUserId.id)
+          }
+          return null
+        }
+      }
+
+      // 开始处理管理员命令
+      if (!!adminSetting[roomId][userId]) {
+        // 当发送者被列为管理员时执行处理
+
+        // 开始处理发送群公告
+        room.announce(text.substring(2))
+      }
+
+
+    }
+
+    console.log(`Room: ${topic} Contact: ${contact.name()} Text: ${text}`)
+    console.log(`senderID: ${contact.id}`)
+  } else {
+    console.log(`Contact: ${contact.name()} Text: ${text}`)
+  }
+
+  return null
+
 }
 
 const bot = new Wechaty({
@@ -59,8 +151,14 @@ const bot = new Wechaty({
    * Learn more about Wechaty Puppet Providers at:
    *  https://github.com/wechaty/wechaty-puppet/wiki/Directory
    */
-  puppet: 'wechaty-puppet-wechat4u',
+  puppet: 'wechaty-puppet-padplus',
 })
+
+// 用来储存管理员信息
+// 开发阶段为了方便直接用变量存储
+// 后期维护时将转移进第三方存储
+var adminSetting: { [key: string]: { [key: string]: any } } = {}
+
 
 bot.on('scan',    onScan)
 bot.on('login',   onLogin)
@@ -70,3 +168,13 @@ bot.on('message', onMessage)
 bot.start()
   .then(() => log.info('StarterBot', 'Starter Bot Started.'))
   .catch(e => log.error('StarterBot', e))
+
+
+
+// TODO 命令头暂定为##,后面要改为可通过命令指定
+// TODO 管理员缓存目前存在变量中,后面要转移进第三方存储中(sqllite?)
+// TODO 现在时间不够,图省事把代码都堆在一个文件里了,后面要梳理一下
+// TODO 目前只实现了代发群公告,后面要添加各种群管功能
+// TODO 原本微信机器人的接龙主持功能可以转移过来
+// TODO 计划添加可供调用的API接口,通过接口可触发群管操作
+
